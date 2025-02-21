650029d3c8692d56b3bd07dd07a8b1469a02b029
ElNotsniw <winstonjosh010604@gmail.com>
2025-02-20 19:44:06 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -65,4 +65,45 @@ class Order {
 const order1 = new Order(501, prod1, 2)             // Output: Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400
 console.log(order1.getOrderDetails())
 
-console.log(prod1.getdetails())                     // Output: Product Name: Laptop, ID: 101, Price: $1200, Stock: 5
+console.log(prod1.getdetails())                     // Output: Product: Laptop, ID: 101, Price: $1200, Stock: 5
+
+
+
+
+
+// Task 3 - Creating an Inventory Class
+
+// Creating an Inventory class to include the products that I created in Task 1
+
+class Inventory {
+    constructor() {
+        this.products = []
+    }
+
+    // Adding products to the array by using .push 
+
+    addProduct(product) {
+        this.products.push(product)
+    }
+
+    // Each of the products in the array are separated and console-logged
+
+    listProducts() {
+        this.products.forEach (product => {
+            console.log(product.getdetails())
+        })
+    }
+}
+
+// Creating a new product to add with prod1
+
+const prod2 = new Product(`TV`, 102, 500, 10)
+
+// Declaring a variable inventory to then add both products and list them out in the console by console-logging them
+
+const inventory = new Inventory()
+
+inventory.addProduct(prod1)                 // Output: Product: Laptop, ID: 101, Price: $1200, Stock: 5
+inventory.addProduct(prod2)                 // Output: Product: TV, ID: 102, Price: $500, Stock: 10
+
+inventory.listProducts()
